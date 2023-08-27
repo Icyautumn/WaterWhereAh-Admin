df24555f7684ffed019569dfd7aec7dbf0ee1d88
Victor Chua <victorchua865@gmail.com>
2023-08-27 12:00:31 +0800

    done table

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,124 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { getWaterCooler } from "./firebase";
+import "./App.css";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { GrStatusGood } from "react-icons/Gr";
+import { RxCrossCircled } from "react-icons/Rx";
+import {
+  Button,
+  Stack,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBTwCygIDQW6Jg_0dUIZJ-CKqbX69ThK2M",
+  authDomain: "water-where-ah.firebaseapp.com",
+  projectId: "water-where-ah",
+  storageBucket: "water-where-ah.appspot.com",
+  messagingSenderId: "19294380273",
+  appId: "1:19294380273:web:008bd30821aaead7ec4950",
+  measurementId: "G-D5WBY03LB3",
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [waterCooler, setWaterCoolers] = useState<any>([]); // Specify the type for waterCooler
+
+  useEffect(() => {
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    const db = getFirestore(app);
+
+    const fetchData = async () => {
+      try {
+        const dataFromServer = await getWaterCooler(db); // Call your getWaterCooler function
+        setWaterCoolers(dataFromServer); // Update the state with fetched data
+        console.log(dataFromServer[0]);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <TableContainer>
+        <Table variant="simple">
+          <TableCaption>Water Cooler</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Bottle Friendly</Th>
+              <Th>Cold</Th>
+              <Th>Hot</Th>
+              <Th>Meta Data</Th>
+              <Th>Approval</Th>
+              <Th>Working</Th>
+              <Th>Latitutde</Th>
+              <Th>Longitude</Th>
+              <Th>Operator</Th>
+              <Th>Remarks</Th>
+              <Th>Child Friendly</Th>
+              <Th>Report Count</Th>
+              <Th>Approval</Th>
+            </Tr>
+          </Thead>
+          {waterCooler.map((item: any) => (
+            <Tr key={item.id}>
+              <Td>{item.bottleFriendly ? "true" : "false"}</Td>
+              <Td>{item.hascold ? "true" : "false"}</Td>
+              <Td>{item.hasHot ? "true" : "false"}</Td>
+              <Td>{item.importMetadata}</Td>
+              <Td>{item.isApproved}</Td>
+              <Td>{item.isWorking ? "true" : "false"}</Td>
+              <Td>{item.location._lat}</Td>
+              <Td>{item.location._long}</Td>
+              <Td>{item.operator}</Td>
+              <Td>{item.remarks}</Td>
+              <Td>{item.reportCount}</Td>
+              <Td>{item.wheelchairFriendly}</Td>
+              <Td>
+                <Stack direction="row" spacing={1}>
+                  <Button
+                    width={"5px"}
+                    paddingRight={"10px"}
+                    leftIcon={<GrStatusGood />}
+                    colorScheme="green"
+                    variant="solid"
+                  ></Button>
+                  <Button
+                    width={"5px"}
+                    paddingLeft={"9px"}
+                    rightIcon={<RxCrossCircled />}
+                    colorScheme="red"
+                    variant="solid"
+                  ></Button>
+                </Stack>
+              </Td>
+            </Tr>
+          ))}
+          <Tfoot>
+            <Tr>
+              <Th>To convert</Th>
+            </Tr>
+          </Tfoot>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, getDocs, Firestore } from 'firebase/firestore/lite';
+import { getFirestore, collection, getDocs, Firestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,9 +23,10 @@ const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 // Get a list of cities from your database
-async function getCities(db: Firestore) {
-    const citiesCol = collection(db, 'cities');
-    const citySnapshot = await getDocs(citiesCol);
-    const cityList = citySnapshot.docs.map(doc => doc.data());
-    return cityList;
+export async function getWaterCooler(db: Firestore) {
+    const waterCooler = collection(db, 'watercoolers');
+    const waterCoolerSnapshot = await getDocs(waterCooler);
+    const waterCoolerList = waterCoolerSnapshot.docs.map(doc => doc.data());
+    console.log(waterCoolerList)
+    return waterCoolerList;
   }
